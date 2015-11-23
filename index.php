78fb249b2027779abdaae76ca1f4e7f048d1e128
Alex <alexis.picot@etu.univ-lyon1.fr>
2015-11-23 17:25:03 +0100

    Test

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 23/11/2015
+ * Time: 17:20
+ */
+?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Acceuil</title>
+</head>
+<body>
+
+</body>
+</html>
