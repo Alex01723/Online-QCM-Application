704651fb47f2cc971c5024c84a59515ff737c04b
Martin <martin.rascle@gmail.com>
2015-12-08 14:13:41 +0100

    Template

--- index.php
+++ index.php
@@ -1,19 +1,40 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Alex
- * Date: 23/11/2015
- * Time: 17:20
- */
-?>
-
-<!doctype html>
-<html lang="fr">
-<head>
+<!DOCTYPE html>
+<html >
+  <head>
     <meta charset="UTF-8">
-    <title>Accueil</title>
-</head>
-<body>
-    <p>Bibi 0 :)</p>
-</body>
+    <title>Calm breeze login screen</title>
+        <link rel="stylesheet" href="css/style.css">
+  </head>
+
+  <body>
+
+    <div class="wrapper">
+	<div class="container">
+		<h1>Bienvenue</h1>
+		
+		<form class="form">
+			<input type="text" placeholder="E-mail">
+			<input type="password" placeholder="Mot de Passe">
+			<button type="submit" id="login-button">Se connecter</button>
+		</form>
+	</div>
+	
+	<ul class="bg-bubbles">
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
+</div>
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+        <script src="js/index.js"></script>
+
+  </body>
 </html>
